d62df1a9997d7fa45834dcc6a9ae06dd701fc801
Jon Wen <90656665+jonathan-wen-1@users.noreply.github.com>
2021-12-04 20:32:09 -0500

    added configure
    for tomcat

--- src/main/java/com/ss/utopia/UsersApplication.java
+++ src/main/java/com/ss/utopia/UsersApplication.java
@@ -2,10 +2,16 @@ package com.ss.utopia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UsersApplication {
+public class UsersApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(UsersApplication.class);
+	}//this was the one we were missing
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApplication.class, args);
 	}
